d648ad22770ef03ffbfb091450db98b3bdf29288
Gabriel Abreu <gabriel.roberto.abreu@outlook.com.br>
2026-09-23 12:03:08 -0300

    feat: configure i18n routing (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,13 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    locales: ["en", "pt"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
